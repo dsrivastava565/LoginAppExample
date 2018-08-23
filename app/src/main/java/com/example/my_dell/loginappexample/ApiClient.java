2e3b80e1fa55acb1e34f917c4bc65b4434f3aaac
my-dell <dsrivastava565@gmail.com>
2018-08-23 23:43:35 +0530

    jjjj

--- app/src/main/java/com/example/my_dell/loginappexample/ApiClient.java
+++ app/src/main/java/com/example/my_dell/loginappexample/ApiClient.java
@@ -1,20 +1,37 @@
 package com.example.my_dell.loginappexample;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    public static final String BASE_URL = "http://127.0.0.1/loginapp/";
+    public static final String BASE_URL = "http://10.21.66.34/loginapp/";
 
 public static Retrofit retrofit = null;
 
-
   public  static Retrofit getApiClient()
   {
+      HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+// set your desired log level
+      logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+      OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+// add your other interceptors …
+
+// add logging as last interceptor
+      httpClient.addInterceptor(logging);  // <-- this is the important line!
+
       if (retrofit==null)
       {
-          retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+          retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+                  .client(httpClient.build())
+                  .build();
 
       }
       return retrofit;
